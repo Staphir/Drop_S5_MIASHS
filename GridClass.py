b0724b4330efb4abb6a4ec61dc744632c2bea451
MaximeD <staphir@gmail.com>
2018-11-09 11:22:43 +0100

    ajout todo et check

--- GridClass.py
+++ GridClass.py
@@ -4,10 +4,10 @@ from ezTK import *
 
 class Grid(Win):
 
-    def __init__(self, rows=8, cols=5, size=64, nbNewBricks=2):
+    def __init__(self, rows=8, cols=5, size=64, nbNewBricks=4):
         Win.__init__(self, title='DROP', bg=('#0066CC'),op=10, key=self.event)
         self.nbBricks = nbNewBricks
-        rows += 3
+        rows += self.nbBricks + 1
         self.rows = rows
         self.cols = cols
         self.colors = ['#0066CC','#CDCDCD', '#FF0000', '#00FF00', '#0000FF']
@@ -18,7 +18,7 @@ class Grid(Win):
 
         for r in range(rows):
             for c in range(cols):
-                if r<4:
+                if r<self.nbBricks + 1:
                     self.grid[r][c].append(Brick(frameGrid, height=size, width=size, bg=(self.colors[0]),bd=0))
                 else:
                     self.grid[r][c].append(Brick(frameGrid, height=size, width=size, bg=(self.colors[1]),bd=0))
@@ -30,9 +30,9 @@ class Grid(Win):
 
         #grille [x][-y] debut en haut a gauche
         self.gridBricks = self[0]
-        self.gridBricks[1][1]['bg'] = self.colors[2]
+        self.gridBricks[self.nbBricks-1][1]['bg'] = self.colors[2]
         #deuxieme ligne deuxieme colonne : initiatisation d'une brique colorée a empiler
-        self.gridBricks[1][2]['bg'] = self.colors[3]
+        self.gridBricks[self.nbBricks-1][2]['bg'] = self.colors[3]
         # deuxieme ligne troisieme colonne : initiatisation d'une brique colorée a empiler
         self.loop()
 
@@ -43,39 +43,44 @@ class Grid(Win):
             self.moveLeft()
         if code == 'Right':
             self.moveRight()
-        #si on actionne la touche bas, gauche ou droite, on renvoie la fonction correspondante
+        if code == 'Up':
+            self.rotate()
+        #si on actionne la touche bas, gauche, droite ou haut, on renvoie la fonction correspondante
+
+    # def rotate(self):
+
 
     def moveLeft(self):
-        #if not self.gridBricks[i][0]['bg'] == self.colors[0] for i in range()
-        #--------------------------------------------------------------------
         if self.begin-1 >= 0:
-            newColors = [[self.gridBricks[1][i + self.begin]['bg']] for i in range(self.end - self.begin + 1)]
+            lNewMax = self.nbBricks-1
+            newColors = [[self.gridBricks[lNewMax][i + self.begin]['bg']] for i in range(self.end - self.begin + 1)]
             for i in range(self.end-self.begin+1):
-                self.gridBricks[1][self.begin-1+i]['bg'] = newColors[i]
-            self.gridBricks[1][self.end]['bg'] = self.colors[0]
+                self.gridBricks[lNewMax][self.begin-1+i]['bg'] = newColors[i]
+            self.gridBricks[lNewMax][self.end]['bg'] = self.colors[0]
             self.begin, self.end = self.begin-1, self.end-1
 
     def moveRight(self):
         if self.end+1 < self.cols:
-            newColors = [[self.gridBricks[1][i + self.begin]['bg']] for i in range(self.end - self.begin + 1)]
+            lNewMax = self.nbBricks-1
+            newColors = [[self.gridBricks[lNewMax][i + self.begin]['bg']] for i in range(self.end - self.begin + 1)]
             for i in range(self.end-self.begin+1):
-                self.gridBricks[1][self.end+1-i]['bg'] = newColors[self.end-self.begin-i]
-            self.gridBricks[1][self.begin]['bg'] = self.colors[0]
+                self.gridBricks[lNewMax][self.end+1-i]['bg'] = newColors[self.end-self.begin-i]
+            self.gridBricks[lNewMax][self.begin]['bg'] = self.colors[0]
             self.begin, self.end = self.begin+1, self.end+1
 
     def moveDown(self):
         colorBase = [self.colors[0], self.colors[1]]
         newColors = [self.colors[2], self.colors[3]]
-        for i in range (4):
+        for i in range (3):
             self.gridBricks[i][self.begin]['bg'] = self.colors[0]
             self.gridBricks[i + 1][self.begin]['bg'] = newColors[0]
 
-        for j in range (4):
+        for j in range (3):
             self.gridBricks[j][self.end]['bg'] = self.colors[0]
             self.gridBricks[j + 1][self.end]['bg'] = newColors[0]
 
-        i=4
-        j=4
+        i=3
+        j=3
         while i < self.rows:
             if not self.gridBricks[i+1][self.begin]['bg'] in colorBase:
                 break
